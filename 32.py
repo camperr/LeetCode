1d52bae963aa2d1632202d52883b53f564dea849
unknown <>
2017-05-03 14:23:21 +0800

    32 accepted

--- 32.py
+++ 32.py
@@ -1,18 +1,27 @@
-def func(s):
-    l_cnt = 0
-    r_cnt = 0
-    i = 0
-    res = 0
-    size = len(s)
-    for i in range(size):
-        if s[i] == ')': r_cnt += 1
-        else: l_cnt += 1
-        if r_cnt > l_cnt or i == size - 1:
-            res = max(res, min(l_cnt, r_cnt) * 2)
-            l_cnt = 0
-            r_cnt = 0
-    return res
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        size = len(s)
+        d = [0 for i in range(size)]
+        for i in range(size):
+            if i == 0 or s[i] == '(':
+                d[i] = 0
+            else:
+                last = i - d[i - 1] - 1
+                if last >= 0 and s[last] == '(':
+                    d[i] = d[i - 1] + 2
+                    if last >= 1:
+                        d[i] += d[last - 1]
+                else:
+                    d[i] = 0
+            res = max(res, d[i])
+        
+        return res
 
 if __name__ == '__main__':
-    s = '()())(()(()))'
+    s = '())(()))'
     print(func(s))
